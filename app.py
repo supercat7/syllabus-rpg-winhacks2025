b9652cb5adc8b262c62574049786abadc75fb66c
supercat7 <superuser3344@protonmail.com>
2025-02-15 14:39:41 -0500

    idk anymore

--- app.py
+++ app.py
@@ -9,10 +9,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/posts/<post_id>')
-def posts(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
 if __name__ == '__main__':
     app.run(debug=True)
