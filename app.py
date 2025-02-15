1074a3e865f87a02ae23bef1f97eee8adfe404a6
supercat7 <superuser3344@protonmail.com>
2025-02-15 14:26:17 -0500

    added script.js

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ import sys
 sys.path.append("./src")
 import json_func
 
-# Initialize the Flask app
 app = Flask(__name__)
-
-# Define a simple route
 @app.route('/')
 def home():
     return render_template('index.html')
-# Run the app
+
 if __name__ == '__main__':
     app.run(debug=True)
