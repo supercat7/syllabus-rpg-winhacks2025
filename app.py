a0c87d1e301046da1669d8cff1001b11b8161698
supercat7 <superuser3344@protonmail.com>
2025-02-15 14:35:59 -0500

    change in index

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ sys.path.append("./src")
 import json_func
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/posts/<post_id>')
+def posts(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
 
 if __name__ == '__main__':
     app.run(debug=True)
