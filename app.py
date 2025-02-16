95721f1b93f4f4627a21efdc1a713bc7e980fd0d
Risheek Jayakumar <risheekofficial04@gmail.com>
2025-02-15 20:23:10 -0500

    use the add.py to execute the program

--- app.py
+++ app.py
@@ -2,15 +2,28 @@ from flask import Flask, render_template, request, jsonify
 import json
 import os
 import sys
+import webbrowser
 sys.path.append("./src")
 from json_func import read_json, write_comp_to_json 
 
 app = Flask(__name__)
 
-@app.route('/')
+#Route for the home page 
+@app.route('/') 
 def index():
     return render_template('index.html')
 
+#Route for the profile page 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+#route for the To-Do-List page
+@app.route('/todo')
+def todo():
+    return render_template('todo.html')
+
+#Route for adding a class
 @app.route('/add_class', methods=['POST'])
 def add_class():
     try:
@@ -28,4 +41,6 @@ def add_class():
         return jsonify({"error": "Failed to add class", "message": str(e)}), 500
 
 if __name__ == '__main__':
+    # Open the browser automatically
+    webbrowser.open_new('http://127.0.0.1:5000')
     app.run(debug=True)
